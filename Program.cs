c6a3435a25002c8fb45161e361c8fd9e759d5999
vishwasng90 <164770369+vishwasng90@users.noreply.github.com>
2024-06-11 14:47:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 1;
+            switch (day)
+            {
+                case 6:
+                    Console.WriteLine("It's Sat means weeken");
+                    break;
+                case 7:
+                    Console.WriteLine("It's Sun means weeken");
+                    break;
+                    default:
+                    Console.WriteLine("It's not weekend");
+                    break;
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
